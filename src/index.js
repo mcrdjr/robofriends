edc07978ca3f2c04ae058dc9e47409575a200b6d
mcrdjr <mcrdjr@sfdc2max.com>
2022-06-06 19:55:45 -0400

    Continued Work on robofriends

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
+import 'tachyons';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1> Hello World </h1>
+    <Hello greeting={'Hello ' + 'React Ninja'}/>
     <App />
   </React.StrictMode>
 );
