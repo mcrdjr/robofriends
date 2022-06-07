57a60b28cd8b29b6853c8e7f5f932a7581586b5e
Michael Rucker <mcrdjr@sfdx2max.com>
2022-06-06 23:04:25 -0400

    Start of Robofriends Code

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt="robots" src='https://robohash.org/mcrdjr?200x200'/>
+            <div>
+                <h2>MCR</h2>
+                <p>mcrdjr@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Hello from './Hello';
+
+import Card from './Card'
 import 'tachyons';
 
 import reportWebVitals from './reportWebVitals';
@@ -10,8 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greeting={'Hello ' + 'React Ninja'}/>
-    <App />
+    <Card />
+    <Card />
+    <Card />
   </React.StrictMode>
 );
 
