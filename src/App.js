af5240f906001e395dd3946156555f8a8d2c06a4
mcrdjr <mcrdjr@sfdc2max.com>
2022-06-07 22:15:41 -0400

    Mre Changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox/>
-        <button class="bw0 br2 bg-dwyl-teal pa2 green fw1 tc ttu tracked">do what you love</button>
-        <CardList robots={robots}/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }    
+    }
+
+    onSearchChange() {
+
+        
+    }
+    render() {
+        return (
+            <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox/>
+            {/* <button class="bw0 br2 bg-dwyl-teal pa2 green fw1 tc ttu tracked">do what you love</button> */}
+            <CardList robots={this.state.robots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
