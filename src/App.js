82bdf3be9c07eabe6daf598b58ac4d0397cd7884
Michael Rucker <mcrdjr@sfdx2max.com>
2022-06-08 00:16:45 -0400

    More Changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: [],
+            robots: [], //was robots
             searchfield: ''
         }   
         //console.log('Constructor'); 
